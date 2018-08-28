4fad5a5aa31d3b742b71d4ff89b04141a131f0e2
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-28 19:20:06 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -1,45 +1,42 @@
-/**
+/**.
  * { item_description }
  */
 import java.util.Scanner;
-/**
+/**.
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution(){
-		/**
-		 * { item_description }
-		 */
-	}
-	/*
-	Do not modify this main function.
-	*/
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);      
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
         long result = power(base,exponent);
         System.out.println(result);
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
-	public static long power(int base, int exponent)
-	{
-		if(exponent != 0){
-			return base * power(base,exponent-1);
-		}
-		else {
-			return 1;
-		}
-		
-	}
+    }
+    /*
+    Need to write the power function and print the output.
+    */
+    public static long power(int base, int exponent)
+    {
+        if(exponent != 0){
+            return base * power(base,exponent-1);
+        }
+        else {
+            return 1;
+        }
+        
+    }
 }
