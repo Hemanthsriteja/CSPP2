3c183b47a61aba086d3abde597b7767e9b634db2
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-28 15:49:00 +0530

    fact

---
+++ cspp2-practice/m2/a11/factorial.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.Math.*;
+
+class factorial
+{
+	public static int fact(int n){
+	if(n == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return n*fact(n-1);
+	}
+}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(fact(n));
+	}
+}
