c70ae8fe2a61f1ff380891f157ef39c91cb6914b
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-28 11:46:18 +0530

    swap

---
+++ cspp2-practice/m1/a12/Swap.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Swap
+{
+    public static void main(String[] args) {
+        
+        
+        Scanner sc = new Scanner(System.in);
+        //Scanner b = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
+        }
+
+
+    }
