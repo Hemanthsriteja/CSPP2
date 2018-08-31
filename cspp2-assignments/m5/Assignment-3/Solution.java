75356847ea566517701cea1f5b1c8bbbd92ef5fc
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-31 18:19:20 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m5/Assignment-3/Solution.java
+++ cspp2-assignments/m5/Assignment-3/Solution.java
@@ -1,18 +1,39 @@
 
-// Given an String, Write a java method that returns the decimal value for the given binary string.
+// Given an String, Write a java method that returns
+// the decimal value for the given binary string.
 
-
+/**.
+ * { item_description }
+ */
 import java.util.Scanner;
-public class Solution
-{/*
-    Do not modify this main function.
-    */
+/**.
+ * Class for solution.
+ */
+public final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args)
+    /**.
+     * { item_description }
+     */
+
+
     {
-        Scanner sc=new Scanner(System.in);
-        int n= sc.nextInt();
-        for(int i=0;i<=n;i++){
-            String s=sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0;i <= n; i++){
+            String s = sc.nextLine();
             int res = 0;
             if(!s.isEmpty()){
                 res = binaryToDecimal(s);//Write binaryToDecimal function
@@ -20,8 +41,19 @@ public class Solution
         }
     }
     }
-    public static int binaryToDecimal(String res) {
-        int dec = Integer.parseInt(res,2);
+    /**.
+     * { function_description }
+     *
+     * @param      res   The resource
+     *
+     * @return     { description_of_the_return_value }
+     */
+
+    public static int binaryToDecimal(final String res) {
+        /**.
+         * { var_description }
+         */
+        int dec = Integer.parseInt(res, 2);
         return dec;
         }
     }
