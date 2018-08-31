26989afb43c86f8e224aaaab2740bb799c18d9e0
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-08-31 18:14:47 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-assignments/m5/Assignment-3/Solution.java
+++ cspp2-assignments/m5/Assignment-3/Solution.java
@@ -5,24 +5,24 @@
 import java.util.Scanner;
 public class Solution
 {/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		for(int i=0;i<=n;i++){
-			String s=sc.nextLine();
-		    int res = 0;
-		    if(!s.isEmpty()){
-			    res = binaryToDecimal(s);//Write binaryToDecimal function
-			    System.out.println(res);
-		}
-	}
-	}
-	public static int binaryToDecimal(String res) {
-		int dec = Integer.parseInt(res,2);
-		return dec;
-		}
-	}
+    Do not modify this main function.
+    */
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        for(int i=0;i<=n;i++){
+            String s=sc.nextLine();
+            int res = 0;
+            if(!s.isEmpty()){
+                res = binaryToDecimal(s);//Write binaryToDecimal function
+                System.out.println(res);
+        }
+    }
+    }
+    public static int binaryToDecimal(String res) {
+        int dec = Integer.parseInt(res,2);
+        return dec;
+        }
+    }
 
