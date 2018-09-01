60c1a817f0baf30cb577718abc6b5bb5663b59b0
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-09-01 11:36:25 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-1/Solution.java
+++ cspp2-assignments/m6/Assignment-1/Solution.java
@@ -1,47 +1,47 @@
-import java.util.Scanner;
-/**
- * Write a java program to find the odd composite numbers
- * between 2 and n where n is given as an input.
- *
- *
- */
-public final class Solution {
+    import java.util.Scanner;
     /**
-    * Empty constructor.
-    */
-    private Solution() {
-        //not used
-    }
-    /**
-     * Prints the odd composite numbers between 2 and n.
+     * Write a java program to find the odd composite numbers
+     * between 2 and n where n is given as an input.
      *
-     * @param      n     n value
+     *
      */
-    static void oddComposites(final int n) {
-    int c = 0;
-    for (int i = 3; i<n ; i++ ) {
-        if(i%2 != 0) {
-            c = 0 ;
-            //System.out.println(c);
-            for (int j = 3; j<i; j++) {
-                if (i%j == 0) {
-                    c++ ;
-                    //System.out.println(c);
+    public final class Solution {
+        /**
+        * Empty constructor.
+        */
+        private Solution() {
+            //not used
         }
-                }if (c > 0) {
-                    System.out.println(i);
+        /**
+         * Prints the odd composite numbers between 2 and n.
+         *
+         * @param      n     n value
+         */
+        static void oddComposites(final int n) {
+        int c = 0;
+        for (int i = 3; i < n; i++ ) {
+            if(i%2 != 0) {
+                c = 0 ;
+                //System.out.println(c);
+                for (int j = 3; j<i; j++) {
+                    if (i%j == 0) {
+                        c++ ;
+                        //System.out.println(c);
+            }
+                    }if (c > 0) {
+                        System.out.println(i);
+                }
             }
         }
+        }
+        /**
+        * main method as driver program.
+        * @param args is the parameter for this method
+        */
+        public static void main(final String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int n = scan.nextInt();
+            oddComposites(n);
+        }
     }
-    }
-    /**
-    * main method as driver program.
-    * @param args is the parameter for this method
-    */
-    public static void main(final String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        oddComposites(n);
-    }
-}
 
