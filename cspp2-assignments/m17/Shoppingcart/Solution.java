486d9213d6dacde1bb4b314432e3b9e9d1c2c701
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-09-14 21:20:37 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m17/Shoppingcart/Solution.java
+++ cspp2-assignments/m17/Shoppingcart/Solution.java
@@ -1,6 +1,18 @@
-import java.util.*;
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 class Solution {
-    public static void main(String[] args) {
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * function_description.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         ShoppingCart shop = new ShoppingCart();
         Scanner sc = new Scanner(System.in);
         int lines = Integer.parseInt(sc.nextLine());
