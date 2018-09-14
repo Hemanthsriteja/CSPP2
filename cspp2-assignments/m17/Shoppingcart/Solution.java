fb10cad56aff95838f0002b23ce79d20db7fa6d6
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-09-14 21:14:52 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m17/Shoppingcart/Solution.java
+++ cspp2-assignments/m17/Shoppingcart/Solution.java
@@ -22,10 +22,10 @@ class Solution {
                 shop.showCart();
                 break;
                 case "totalAmount":
-                System.out.println("TotalAmount: "+ shop.getTotalAmount());
+                System.out.println("totalAmount: "+ shop.getTotalAmount());
                 break;
                 case "payableAmount":
-                System.out.println("payable amount: "+ shop.getPayableAmount());
+                System.out.println("Payable amount: "+ shop.getPayableAmount());
                 break;
                 case "remove":
                 String[] tokens12 = input[1].split(",");
