d212c7813d1010e032a5abb9210d710cbafb9c95
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-09-15 11:31:16 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Solution.java
@@ -63,10 +63,7 @@ public final class Solution {
         // write your code here to read the questions from the console
         // tokenize the question line and create the question object
         // add the question objects to the quiz class
-        String str = s.nextLine();
-        for (int i = 0; i < questionCount; i++) {
-            String[] input = s.nextLine().split(":");
-        }
+
 
     }
 
