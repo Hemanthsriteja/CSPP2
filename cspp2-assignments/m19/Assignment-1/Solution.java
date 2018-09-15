8a209462deeb7030893b900114d4eadf5e38dea9
Hemanthsriteja <ramanchi.hemanth@msitprogram.net>
2018-09-15 11:33:05 +0530

    "check_style_score -> 1 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Solution.java
@@ -63,7 +63,9 @@ public final class Solution {
         // write your code here to read the questions from the console
         // tokenize the question line and create the question object
         // add the question objects to the quiz class
-
+        if (questionCount == 0) {
+            System.out.println("Quiz does not have questions");
+        }
 
     }
 
